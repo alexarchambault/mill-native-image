f2733ab45d9f3e406bc8c07ec0081afbb7a2133c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-06-10 13:54:25 +0200

    Update client4:core to 4.0.25 (#186)

--- build.mill.scala
+++ build.mill.scala
@@ -67,7 +67,7 @@ object Scala {
 object Versions {
   def osLib = "0.11.8"
   def ujson = "4.4.3"
-  def sttp  = "4.0.23"
+  def sttp  = "4.0.25"
 }
 
 object plugin      extends Cross[PluginModule](millBinaryVersions)
